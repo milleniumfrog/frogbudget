7745538103d38fdd0fec5f7d7a79212400228231
René Schwarzinger <web@milleniumfrog.de>
2019-01-15 23:09:13 +0100

    add logo

--- configs/prod.webpack.js
+++ configs/prod.webpack.js
@@ -2,7 +2,8 @@ const merge = require('webpack-merge');
 const common = require('./common.webpack');
 const DefinePlugin = require('webpack').DefinePlugin;
 const VERSION = process.env.npm_package_version;
-
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
 	devtool: 'source-map',
@@ -10,6 +11,22 @@ module.exports = merge(common, {
 		new DefinePlugin({
 			PRODUCTION: JSON.stringify( true ),
 			VERSION: JSON.stringify(VERSION)
+		}),
+		new FaviconsWebpackPlugin({
+			logo: path.resolve(__dirname, "../src/assets/logo.png"),
+			lang: "de-DE",
+			appName: 'frogbudget',
+			appShortName: 'f-budget',
+			theme_color: "#fff",
+			display: 'standalone',
+			prefix: 'icons-[hash]/',
+			start_url: '/',
+			icons: {
+				android: true,
+				favicons: true,
+				appleIcon: true,
+				appleStartup: true
+			}
 		}),
 	],
 
