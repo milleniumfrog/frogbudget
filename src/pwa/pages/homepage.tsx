45900b6c995b732c23616008a953602579ac3ef1
René Schwarzinger <web@milleniumfrog.de>
2019-02-20 17:06:44 +0100

    add menu on homepage

--- src/pwa/pages/homepage.tsx
+++ src/pwa/pages/homepage.tsx
@@ -3,31 +3,26 @@ import { connect } from "react-redux";
 import { mapStateToProps } from "../providers/redux/connector";
 import { Page, Card, List, ListItem, Row, Col, Fab, Icon, Button, Splitter, SplitterSide, SplitterContent } from 'react-onsenui';
 import { Entry } from '../../types/entry';
-import { withRouter } from 'react-router';
+import { withRouter, Redirect } from 'react-router';
 import { removeEntry } from '../providers/database/entries';
 import { removeActionCreator } from '../providers/redux/actions/entries';
 import { store } from '../providers/redux/store';
 import { logger } from '../providers/logger';
 import { generateEntriesFromRepeats } from '../providers/universal/entries_and_repeats';
-
+declare const VERSION: string;
 interface Props {
 	entries: Entry[]
 }
 interface State {
 	total: number,
 	slice: number;
+	openMenu: boolean;
+	redirect?: string;
 }
 
 let counter = 0;
 
 class HomePage extends React.Component<Props, State>{
-
-	static Fab = withRouter(({ history }) => {
-		return (
-			<Fab style={{ position: "fixed", bottom: "10px", right: "10px" }} onClick={() => history.push("/add")}>
-				<Icon icon={{ default: 'fa-plus' }} size={{ default: 32 }} />
-			</Fab>)
-	})
 
 	constructor(props: Props) {
 		logger.construct("homepage");
@@ -35,68 +30,117 @@ class HomePage extends React.Component<Props, State>{
 		this.state = {
 			total: 0,
 			slice: 0,
+			openMenu: false
 		}
 		generateEntriesFromRepeats();
+		this.MenuLeft = this.MenuLeft.bind(this);
 	}
 
 	render() {
+		if(this.state.redirect)
+			return(<Redirect to={this.state.redirect} />)
 		logger.info("RENDER homepage");
 		let arr: string[] = [];
 		return (
-			<Page>
-				<Card>
-					{calcTotal(this.props.entries).toFixed(2)} €
-				</Card>
+			<Splitter>
+				<SplitterSide
+					side="left"
+					collapse={true}
+					width={200}
+					isOpen={this.state.openMenu}
+					swipeable={true}>
+					<Page>
+						<this.MenuLeft />
+					</Page>
+				</SplitterSide>
+				<SplitterContent>
+					<Page>
+						<Card>
+							<Row>
+								<Col>
+									<Button onClick={() => {this.setState({openMenu: true})}}>
+										<Icon size={{default: 32}} icon={{default: 'ion-navicon'}}/>
+									</Button>
+								</Col>
+								<Col>
+									{calcTotal(this.props.entries).toFixed(2)} €
+								</Col>
+							</Row>
+						</Card>
 
-				<List 
-					dataSource={this.props.entries.slice(this.state.slice*20, 20*(1+this.state.slice))}
-					renderRow={(row: Entry) => {
-						if (arr.indexOf(new Date(row.date).toDateString()) < 0) {
+						<List
+							dataSource={this.props.entries.slice(this.state.slice * 20, 20 * (1 + this.state.slice))}
+							renderRow={(row: Entry) => {
+								if (arr.indexOf(new Date(row.date).toDateString()) < 0) {
 
-						}
-						return (
-							<React.Fragment key={row.id || `tmp_${++counter}`}>
-								{
-									arr.indexOf(new Date(row.date).toDateString()) < 0 &&
-									(() => {
-										arr.push(new Date(row.date).toDateString())
-										return true;
-									})() &&
-									<ListItem className="homepage_date" modifier="nodivider">
-										<Row><Col>{new Date(row.date).toDateString()}</Col></Row>
-									</ListItem>
 								}
-								<ListItem modifier="longdivider">
-									<Row>
-										<Col>
-											{row.category}
-										</Col>
-										<Col width="100px">
-											{row.value}€
+								return (
+									<React.Fragment key={row.id || `tmp_${++counter}`}>
+										{
+											arr.indexOf(new Date(row.date).toDateString()) < 0 &&
+											(() => {
+												arr.push(new Date(row.date).toDateString())
+												return true;
+											})() &&
+											<ListItem className="homepage_date" modifier="nodivider">
+												<Row><Col>{new Date(row.date).toDateString()}</Col></Row>
+											</ListItem>
+										}
+										<ListItem modifier="longdivider">
+											<Row>
+												<Col>
+													{row.category}
+												</Col>
+												<Col width="100px">
+													{row.value}€
 									</Col>
-										<Col width="40px">
-											<Button onClick={() => { removeEntry(row); store.dispatch(removeActionCreator(row)) }}>
-												<Icon icon="md-delete" />
-											</Button>
-										</Col>
-									</Row>
-								</ListItem>
-							</React.Fragment>
-						)
-					}}
-				/>
-				<Card>
-					<Button onClick={() => {this.setState({slice: this.state.slice-1})}}>
-						Vorherigen 20
+												<Col width="40px">
+													<Button onClick={() => { removeEntry(row); store.dispatch(removeActionCreator(row)) }}>
+														<Icon icon="md-delete" />
+													</Button>
+												</Col>
+											</Row>
+										</ListItem>
+									</React.Fragment>
+								)
+							}}
+						/>
+						<Card>
+							<Button onClick={() => { this.setState({ slice: this.state.slice > 0 ? this.state.slice - 1 : this.state.slice }) }}>
+								Vorherigen 20
 					</Button>
-					<Button onClick={() => {this.setState({slice: this.state.slice+1})}}>
-						Nächsten 20
+							<Button onClick={() => { this.setState({ slice: this.state.slice + 1 }); window.scrollTo(0, 0) }}>
+								Nächsten 20
 					</Button>
-				</Card>
-				<HomePage.Fab />
-			</Page>
+						</Card>
+						<Fab style={{ position: "fixed", bottom: "10px", right: "10px" }} onClick={() => this.setState({redirect: '/add'})}>
+							<Icon icon={{ default: 'fa-plus' }} size={{ default: 32 }} />
+						</Fab>
+					</Page>
+				</SplitterContent>
+			</Splitter>
 		)
 	}
+
+	MenuLeft() {
+		return(
+			<List dataSource={menu}
+				renderRow={(val: [string, string | undefined]) => {
+					return(
+						<ListItem key={"tmp_" + (++counter)}
+							onClick={() => {
+								if(val[1])
+									this.setState({redirect: val[1]})
+							}}
+						>
+							{val[0]}
+						</ListItem>
+					);
+				}}
+			/>
+		);
+	}
+
 }
 
 function calcTotal(entry_arr: Entry[]) {
@@ -106,5 +150,10 @@ function calcTotal(entry_arr: Entry[]) {
 	}
 	return total;
 }
+
+const menu: [string, string | undefined][] = [
+	['version: ' + VERSION, undefined]
+];
+
 
 export const ConnectedHomepage = connect(mapStateToProps, null)(HomePage);
